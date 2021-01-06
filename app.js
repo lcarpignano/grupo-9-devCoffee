ccd8d2d7f5bc6a323377c45d95fac2167abf01a6
lcarpignano <lcarpignano@gmail.com>
2021-01-06 19:00:19 -0300

    register login and modif css

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
 app.listen(3030, console.log('Ya puedes ir a http://localhost:3030'));
 
 

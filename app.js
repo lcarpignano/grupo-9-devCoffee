caf74c333f21bdc8f78a03c85e5a7efb1acc0a2b
mauroamestoy <mauroamestoy@gmail.com>
2021-01-07 21:16:24 +0100

    product detail page finish 7-1-2020

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/catalogo', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productcatalog.html'))
 })
 
+app.get('/producto', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/product-detail.html'))
+})
+
 app.listen(3030, console.log('Ya puedes ir a http://localhost:3030'));
 
 

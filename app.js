108493ba7cb238faaae7ae56673aba97c8d4ec5f
lcarpignano <lcarpignano@gmail.com>
2021-01-06 21:02:34 -0300

    resgiter login and css changes

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.get('/login.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
 })
 
+app.get('/catalogo', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productcatalog.html'))
+})
+
 app.listen(3030, console.log('Ya puedes ir a http://localhost:3030'));
-
-
-
 
